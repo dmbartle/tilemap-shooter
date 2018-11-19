01b34fd6c36a2198b6c481855fcf97e2aa8565f7
dmbartle <daniel.m.bartle@gmail.com>
2018-11-19 21:12:02 +0000

    Adding the code for the tilemap shooter zomvie game

---
+++ maps/tilesheet_complete.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tilesheet_complete" tilewidth="64" tileheight="64" tilecount="540" columns="27">
+ <image source="../img/tilesheet_complete.png" width="1728" height="1280"/>
+</tileset>
